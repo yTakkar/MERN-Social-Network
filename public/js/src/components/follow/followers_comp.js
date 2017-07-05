2e75f772e1a9df96da3a3b1800b0f0c9542f9537
Takkar <www.shtakkar@gmail.com>
2017-07-05 17:35:58 +0530

    Some style changes

--- public/js/src/components/follow/followers_comp.js
+++ public/js/src/components/follow/followers_comp.js
@@ -4,24 +4,24 @@ import Follower_items from './follower_items_comp'
 export default class Followers extends React.Component{
     render(){
         let { dispatch, close, followers } = this.props
-
+        
         return(
             <div class='followers modal modal_big' >
                 <div className="fer_header modal_header">
-                        <span>Followers</span>
+                    <span>Followers</span>
+                </div>
+                <div className="fer_middle modal_middle">
+                    <div className="modal_main">
+                        {
+                            followers.map(ff => {
+                                return <Follower_items key={ff.follow_id} {...ff} dispatch={dispatch} />
+                            })
+                        }
                     </div>
-                    <div className="fer_middle modal_middle">
-                        <div className="modal_main">
-                            {
-                                followers.map(ff => {
-                                    return <Follower_items key={ff.follow_id} {...ff} dispatch={dispatch} />
-                                })
-                            }
-                        </div>
-                    </div>
-                    <div className="fer_bottom modal_bottom">
-                        <a href='#' className='fer_cancel pri_btn' onClick={e => close(e, "followers") } >Close</a>
-                    </div>
+                </div>
+                <div className="fer_bottom modal_bottom">
+                    <a href='#' className='fer_cancel pri_btn' onClick={e => close(e, "followers") } >Close</a>
+                </div>
             </div>
         )
     }
