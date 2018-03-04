1c71d729c6e66c06fc84fa28adfe2c40fd0633fe
yTakkar <www.shtakkar@gmail.com>
2018-03-05 00:08:09 +0530

    Updated babel for it work well with latest version of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,15 @@ module.exports = {
         path: path.join(__dirname,"/public/js/dist/"),
         filename: "bundle.js"
     },
+    mode: 'development',
+    performance: {
+        hints: false,
+    },
     module: {
         rules: [
-            { 	
+            {
 				test: /\.js$/,
-                exclude: /node_modules/, 
+                exclude: /node_modules/,
                 loader: "babel-loader",
                 query: {
                     "presets": ["es2015", "react", "stage-0"],
